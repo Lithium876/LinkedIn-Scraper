898388838d25e47843fb67bef920cb7a4e950997
Lomar Lilly <lithiumlilly3@gmail.com>
2016-06-25 11:38:44 -0600

    Fixes #1

--- LinkedInS.py
+++ LinkedInS.py
@@ -1,10 +1,13 @@
-import time, os, re
+import timeit, time, os, re
 from os import system
 from selenium import webdriver
 from easygui import passwordbox
 from xlwt import Workbook
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+
+new_tab=webdriver.PhantomJS("C:\Program Files (x86)\phantomjs-2.1.1-windows\phantomjs-2.1.1-windows\\bin\phantomjs.exe")
+new_tab.set_window_size(1024, 768)
 
 name=[]
 size=[]
@@ -16,7 +19,7 @@ a = re.compile('&page_num=\d+')
 b = re.compile('&page_num=\d+&')
 
 def write(name,col1, location, col2, size, col3, industry, col4):
-    file=input("\n\nEnter file name: ")
+    file=input("\nEnter file name: ")
     wb = Workbook()
     sheet1 = wb.add_sheet('Sheet 1')
     sheet1.write(0,0,"Company Name")
@@ -36,54 +39,63 @@ def write(name,col1, location, col2, size, col3, industry, col4):
     wb.save(file+'.xls')
 
 def scrape(url):
-	print("Scarping...\n")
-	for x in range(0,len(url)):
-		browser.get(url[x])
-		try:
-			browser.find_element_by_xpath("""//*[@id="stream-about-section"]/div[2]/button[1]/span""").click()
-		except:
-			print("Not there")
-		try:										
-			name1 = browser.find_element_by_xpath("""//*[@id="stream-promo-top-bar"]/div[2]/div[1]/div[1]/div/h1/span""")
-			name.append(name1.text)
-		except:
-			name.append("n/a")
-			print("Error")
-		try:	
-			industry1 = browser.find_element_by_xpath("""//*[@id="stream-about-section"]/div[2]/div[2]/ul/li[2]/p""")
-			industry.append(industry1.text)
-		except:
-			industry.append("n/a")
-			print("Error")
-		try:	
-			location1 = browser.find_element_by_xpath("""//*[@id="stream-about-section"]/div[2]/div[2]/ul/li[4]/p""")
-			location.append(location1.text)
-		except:
-			location.append("n/a")
-			print("Error")
-		try:	
-			size1 = browser.find_element_by_xpath("""//*[@id="stream-about-section"]/div[2]/div[2]/ul/li[5]/p""")
-			size.append(size1.text)
-		except:
-			size.append("n/a")
-			print("Error")
+	new_tab.get(url)
+	try:
+		new_tab.find_element_by_xpath("""//*[@id="stream-about-section"]/div[2]/button[1]/span""").click()
+	except:
+		print("Not there")
+	try:										
+		name1 = new_tab.find_element_by_xpath("""//*[@id="stream-promo-top-bar"]/div[2]/div[1]/div[1]/div/h1/span""")
+		name.append(name1.text)
+	except:
+		name.append("n/a")
+		print("Error")
+	try:	
+		industry1 = new_tab.find_element_by_xpath("""//*[@id="stream-about-section"]/div[2]/div[2]/ul/li[2]/p""")
+		industry.append(industry1.text)
+	except:
+		industry.append("n/a")
+		print("Error")
+	try:	
+		location1 = new_tab.find_element_by_xpath("""//*[@id="stream-about-section"]/div[2]/div[2]/ul/li[4]/p""")
+		location.append(location1.text)
+	except:
+		location.append("n/a")
+		print("Error")
+	try:	
+		size1 = new_tab.find_element_by_xpath("""//*[@id="stream-about-section"]/div[2]/div[2]/ul/li[5]/p""")
+		size.append(size1.text)
+	except:
+		size.append("n/a")
+		print("Error")
 
 def crawl(curr,max_page,url):
-	os.system('cls')
-	page = curr
-	while page <= max_page:
-  		browser.get(url+"&page_num="+str(page))
-  		print("Page: "+str(page))
-  		url_links =[]
-  		for i in range(1,11):
-  			links = browser.find_elements_by_xpath("""//*[@id="results"]/li["""+str(i)+"""]/div/h3/a""")
-	  		for link in links:
-	  			url_links.append(link.get_attribute('href'))
-	  	scrape(url_links)
-	  	page+=1
-	write(name,0, location, 1, size, 2, industry, 3)
-	print("Success!")
-	input()
+	while True:
+		try:
+			os.system('cls')
+			page = curr
+			while page <= max_page:
+		  		browser.get(url+"&page_num="+str(page))
+		  		print("Page: "+str(page))
+		  		print("Scarping...\n")
+		  		start=timeit.default_timer()
+		  		url_links =[]
+		  		for i in range(1,11):
+		  			links = browser.find_elements_by_xpath("""//*[@id="results"]/li["""+str(i)+"""]/div/h3/a""")
+			  		for link in links:
+			  			url_links.append(link.get_attribute('href'))
+			  			scrape(url_links[i-1])
+			  	estimate=timeit.default_timer() - start
+			  	print("\n\n[+]Scarping Page "+str(page)+" Completed\n")
+			  	page+=1
+			  	print("Estimated Time: "+ str(round(estimate,3)))
+			write(name,0, location, 1, size, 2, industry, 3)
+			print("Success!")
+			input()
+			break
+		except:
+			print("Something went wrong, try again")
+			search()
 
 def login(email,passwrd):
 	global browser 
@@ -91,13 +103,17 @@ def login(email,passwrd):
 	browser.set_window_size(1024, 768)
 	browser.implicitly_wait(600)
 	browser.get("https://linkedin.com/uas/login")
-	browser.find_element_by_id("session_key-login").send_keys(email + Keys.TAB)
-	browser.find_element_by_id("session_password-login").send_keys(passwrd + Keys.RETURN)
-	print("\n[+] Success! Logged In, Bot Starting!")
-	time.sleep(1)
-	os.system('cls')
-	search()
-
+	try:
+		browser.find_element_by_id("session_key-login").send_keys(email + Keys.TAB)
+		browser.find_element_by_id("session_password-login").send_keys(passwrd + Keys.RETURN)
+		print("\n[+] Success! Logged In, Bot Starting!")
+		time.sleep(1)
+		os.system('cls')
+		search()
+	except:
+		print("[-] Error In Login, Incorrect Email or Password")
+		Main()
+	
 def search():
 	while True:
 		try:
