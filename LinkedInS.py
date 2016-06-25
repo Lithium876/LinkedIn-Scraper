5daa5896591de826ea07bb4ebae9f4e4eba026a5
Lomar Lilly <lithiumlilly3@gmail.com>
2016-06-25 12:36:23 -0600

    Fixes #2

--- LinkedInS.py
+++ LinkedInS.py
@@ -17,6 +17,7 @@ system("title "+"LinkedIn Bot")
 
 a = re.compile('&page_num=\d+')
 b = re.compile('&page_num=\d+&')
+c = '+ employees'
 
 def write(name,col1, location, col2, size, col3, industry, col4):
     file=input("\nEnter file name: ")
@@ -58,15 +59,19 @@ def scrape(url):
 		print("Error")
 	try:	
 		location1 = new_tab.find_element_by_xpath("""//*[@id="stream-about-section"]/div[2]/div[2]/ul/li[4]/p""")
-		location.append(location1.text)
+		if c in location1.text:
+			location.append("n/a")
+		else:
+			location.append(location1.text)
 	except:
-		location.append("n/a")
 		print("Error")
 	try:	
 		size1 = new_tab.find_element_by_xpath("""//*[@id="stream-about-section"]/div[2]/div[2]/ul/li[5]/p""")
-		size.append(size1.text)
+		if c not in size1.text:
+			size.append("n/a")
+		else:
+			size.append(size1.text)
 	except:
-		size.append("n/a")
 		print("Error")
 
 def crawl(curr,max_page,url):
